310599763ef602d5c54a729708e54de5041cfc13
aniket <aniket.umbc@gmail.com>
2023-03-19 18:26:22 +0530

    Updates

---
+++ precache-manifest.2215b25476d4b09652aa364a27e54e87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b7b1125305cb2683f61eec99f05aae",
+    "url": "/React-todo-list/index.html"
+  },
+  {
+    "revision": "49a8491f6bb66c9c64f4",
+    "url": "/React-todo-list/static/css/main.74911bce.chunk.css"
+  },
+  {
+    "revision": "fb70dd07ee429ff3ff55",
+    "url": "/React-todo-list/static/js/2.8e5fa95e.chunk.js"
+  },
+  {
+    "revision": "49a8491f6bb66c9c64f4",
+    "url": "/React-todo-list/static/js/main.c38b82e6.chunk.js"
+  },
+  {
+    "revision": "74c8b818b0920c03cfb6",
+    "url": "/React-todo-list/static/js/runtime-main.6dfe4b22.js"
+  }
+]);
